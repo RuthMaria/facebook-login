a1fc5447998246d5cf1f1a6886601741f7bdf1d2
RuthMaria <ruth-15rms@hotmail.com>
2020-06-18 21:21:39 -0300

    criado o gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.render("index.html")
+})
+
+app.listen(3000, () => {
+    console.log("Conectado")
+})
